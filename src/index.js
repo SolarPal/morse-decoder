16a07583c1d41c200d1dbab615954d78020519fa
SolarPal <tumaf@mail.ru>
2023-06-21 21:41:56 +0300

    feat: implement the function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	const charSize = 10;
+	let chars = [];
+	for (let i = 0; i < expr.split("").length / charSize; i += 1) {
+		chars.push(expr.split("").slice(i * charSize, i * charSize + charSize));
+	}
+	
+	function readChar(ch, mapKV) {
+		if (ch[0] === "*") {
+			return " ";
+		} else {
+			let resCh = "";
+			for (let i = 0; i < ch.length; i += 2) {
+				if (ch[i] + ch[i + 1] === "00") {
+					continue;
+				} else if (ch[i] + ch[i + 1] === "10") {
+					resCh += ".";
+				} else if (ch[i] + ch[i + 1] === "11") {
+					resCh += "-";
+				}
+			}
+			return mapKV[resCh];
+		}
+	}
+	
+	let resExpr = "";
+	
+	for (let i = 0; i < chars.length; i += 1) {
+		resExpr += readChar(chars[i], MORSE_TABLE);
+	}
+	
+	return resExpr;
 }
 
 module.exports = {
